fb37b263160aeaf2ec082cf33c61826723b57c7d
arr19422 <arr19422@uvg.edu.gt>
2021-03-20 17:38:41 -0600

    Api funcional

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 module.exports = {
     port: process.env.PORT || 3000,
     db: process.env.PG || 'mongodb://pariet:admin@ds243049.mlab.com:43049/pariet',
-    SECRET_TOKEN: 'nodenoseexplicasolo'
 }
