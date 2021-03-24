c64d59fa88947e85c89dadfa65f49d66c6c43507
arr19422 <arr19422@uvg.edu.gt>
2021-03-24 14:46:49 -0600

    Rutas listas

--- controllers/controler.js
+++ controllers/controler.js
@@ -67,9 +67,9 @@ function postStream(req, res) {
 }
 
 function updateUserSub(req, res) {
-    const { id_usuario, fecha } = req.body
-    config.pool.query("UPDATE Usuario SET premium = 'Si', fecha = $1 WHERE id_usuario = $2",
-        [fecha, parseInt(id_usuario)], (err, results) => {
+    const { id_usuario, fecha_suscripcion } = req.body
+    config.pool.query("UPDATE Usuario SET premium = 'Si', fecha_suscripcion = $1 WHERE id_usuario = $2",
+        [fecha_suscripcion, parseInt(id_usuario)], (err, results) => {
             if (err) {
                 throw err
             }
